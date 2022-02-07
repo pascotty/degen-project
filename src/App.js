13affefe913fb7dd783a58fb0f4d2baa2c74cdd0
pascotty <remythedoge@gmail.com>
2022-02-07 17:26:45 -0600

    Started code for 'model', need to set up database

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import * as tf from '@tensorflow/tfjs'; 
 
 function App() {
+  //Put CNN CODE HERE
+
+  const [model, setModel] = useState();
+
+  async function loadModel() {
+    try {
+      const model = await cocoSsd.load();
+      setModel(model);
+      console.log("set loaded Model");
+    } 
+    catch (err) {
+      console.log(err);
+      console.log("failed load model");
+    }
+  }
+  useEffect(() => { tf.ready().then(() => { loadModel(); }); }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
